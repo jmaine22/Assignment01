efffdb385c2334d738b170d5910b146f1972f529
Jmaine22 <jmaine22@gmail.com>
2021-02-17 23:29:31 -0800

    Assignment01

---
+++ js/script.js
@@ -0,0 +1,100 @@
+// STEP 1
+let someMonth;
+//function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+
+// STEP 2
+3 + 4
+"Hello Zak"
+true
+null
+
+// STEP 3
+let num;
+let pie;
+
+// STEP 4
+let firstName; let lastName; let address; let city; let state; let zipCode;
+let age; let _isSource; let contact;
+
+// STEP 5
+firstName = "Jermaine"; lastName = "Deloach"; state = "CA";
+
+// STEP 6
+let answer = 3 + " Is Ryu favorite Number";
+console.log(answer);
+
+// STEP 7
+let response = "One boolean is " + true;
+document.write(response);
+let responeTwo = 3 + true;
+document.write(responeTwo); 
+
+// STEP 8
+let someString = `Who once said, Only two things are infinite, the universe and
+ human stupidity, and I'm not sure about the former.`;
+ window.console.log(someString);
+
+// STEP 9
+let responsive = window.console.log(null);
+let unknown;
+window.console.log(unknown);
+
+// STEP 10
+let car = "Fiat";
+let voice;
+window.console.log(+'-3', +3, +true, +car, +voice);
+
+// STEP 11
+alert('Hello ' +  'Jermaine Deloach'+ ', welcome to the JavaScript class!');
+
+// STEP 12
+let names = 'Jermaine Deloach';
+alert('Hello ' +  names + ', welcome to the JavaScript class!');
+
+// STEP 13
+let course = "JavaScript";
+alert('Hello ' +  names + ', welcome to the' +course + 'class!');
+
+// STEP 14
+alert('Hello ' +  names + ',\n welcome to the' +course + 'class!');
+
+// STEP 15
+names = prompt('What is your name?');
+
+// STEP 16
+course = prompt('What class are you taking');
+
+// STEP 17
+let x = 10;
+let y = 20;
+alert(x + y);
+
+// STEP 18
+x = 20;
+x = x + 20;
+window.console.log(x)
+
+// STEP 19
+x = 20;
+x = x * 5;
+window.console.log(x);
+
+// STEP 20
+x = 20 % 3;
+x = x / 1;
+window.console.log(x);
+
+// STEP 21
+let z = prompt("First Number"); 
+let w = prompt("Second Number");
+let evaluate = z > w || z == w;
+window.console.log(evaluate);
+
+// STEP 22
+ z = prompt("First Number"); 
+ w = prompt("Second Number");
+evaluate = z > w && z != w;
+window.console.log(evaluate);
